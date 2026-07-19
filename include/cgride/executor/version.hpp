15392bbcaf2543670587a710ab39f0d6c3cb310b
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-07-19 13:27:43 +0300

    Bump version to 0.1.1

--- include/cgride/executor/version.hpp
+++ include/cgride/executor/version.hpp
@@ -33,7 +33,7 @@ namespace cgride::executor
   /**
    * @brief Patch version of the Cgride executor module.
    */
-  inline constexpr int version_patch = 0;
+  inline constexpr int version_patch = 1;
 
   /**
    * @brief Public API version of the Cgride executor module.
@@ -43,7 +43,7 @@ namespace cgride::executor
   /**
    * @brief Human-readable version string.
    */
-  inline constexpr std::string_view version_string = "0.1.0";
+  inline constexpr std::string_view version_string = "0.1.1";
 
 } // namespace cgride::executor
 
